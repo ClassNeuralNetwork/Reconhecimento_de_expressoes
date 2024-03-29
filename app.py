8cc57c65b859eb8c806bf54ab5a72fe3a5ac823d
nicolinhaaaaaaas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2024-03-29 07:23:29 -0300

    tentando arrumar o deploy do streamlit

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ import streamlit as st
 from tensorflow import keras
 from keras.models import model_from_json
 from keras.preprocessing import image
-from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration, WebRtcMode
 from tensorflow.keras.preprocessing.image import img_to_array
 
 
 # load model
 emotion_dict = {0:'Raiva', 1:'Nojo', 2:'Medo', 3 :'Feliz', 4: 'Neutro', 5:'Triste', 6: 'Surpresa'}
-
 # load json and create model
 json_file = open('modelo/emotion_modelcnn90valAcc.json', 'r')
 loaded_model_json = json_file.read()
@@ -26,6 +25,15 @@ try:
 except Exception:
     st.write("Error loading cascade classifiers")
 
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
 class Faceemotion(VideoTransformerBase):
     def __init__(self):
@@ -42,24 +50,24 @@ class Faceemotion(VideoTransformerBase):
         if self.frame_count % 10 == 0:
             self.faces = face_cascade.detectMultiScale(
                 image=img_gray, scaleFactor=1.3, minNeighbors=5)
-            
+
         for (x, y, w, h) in self.faces:
             cv2.rectangle(img=img, pt1=(x, y), pt2=(
                 x + w, y + h), color=(255, 0, 0), thickness=2)
-            
+
             roi_gray = img_gray[y:y + h, x:x + w]
             roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            
+
             if np.sum([roi_gray]) != 0:
                 roi = roi_gray.astype('float') / 255.0
                 roi = img_to_array(roi)
                 roi = np.expand_dims(roi, axis=0)
-                
+
                 prediction = classifier.predict(roi)[0]
                 maxindex = int(np.argmax(prediction))
                 finalout = emotion_dict[maxindex]
                 output = str(finalout)
-                
+
             label_position = (x, y)
             cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
@@ -97,7 +105,7 @@ def main():
     elif choice == "Reconhecimento de face por Webcam":
         st.header("Webcam ao vivo")
         st.write("Clique no START para ativar a detecção de face e reconhecimento de emoções")
-        webrtc_streamer(key="example",
+        webrtc_streamer(key="example", mode=WebRtcMode.SENDRECV, rtc_configuration=RTC_CONFIGURATION,
                         video_processor_factory=Faceemotion)
         
         st.write("O modelo de reconhecimento de expressões faciais foi treinado com 7 classes de emoções: Raiva, Nojo, Medo, Feliz, Neutro, Triste e Surpresa.")
